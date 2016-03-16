3931395bba7b808853d30c09ca5b6f6137db6f4d
pkx0128 <764670547@qq.com>
2016-03-16 21:37:34 +0800

    增加ip地址输入非空判断

--- check/check/ViewController.swift
+++ check/check/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Pankx on 16/3/16.
 //  Copyright © 2016年 Pankx. All rights reserved.
-// //218.4.255.255
+// 218.4.255.255
 
 import UIKit
 import Alamofire
@@ -14,8 +14,17 @@ class ViewController: UIViewController,UIAlertViewDelegate {
     @IBOutlet weak var input_IP: UITextField!
     //设置button关联
     @IBAction func check(sender: AnyObject) {
-        let ip = input_IP.text;
-        checkData(ip!);
+        let ip = input_IP.text
+         if ip != ""{
+         checkData(ip!);
+            print(ip);
+        }else{
+            let alc = UIAlertView();
+            alc.message = "请输入合法的IP地址！";
+            alc.addButtonWithTitle("OK");
+            alc.show();
+        }
+        
     }
 
     override func viewDidLoad() {
